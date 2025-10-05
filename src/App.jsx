1385f68d8bbcdcced5cb7d5f2d3811d205745688
nove23111 <145318041+nove23111@users.noreply.github.com>
2025-10-06 05:37:52 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { forceChakraDarkTheme } from './utils/utils';
-import DotGridDemo from './demo/Backgrounds/DotGridDemo';
+import LaserFlowDemo from './demo/Animations/LaserFlowDemo';
 
 function AppContent() {
   return (
     <>
-      <DotGridDemo />
+      <LaserFlowDemo />
     </>
   );
 }
